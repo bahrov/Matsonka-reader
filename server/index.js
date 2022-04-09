0ae129726b1aee45512133f328a2e3b28581cdfb
Maksym Bahrov <m.bahrov@gmail.com>
2022-04-09 20:57:35 +0300

    up a new server and react front end

--- server/index.js
+++ server/index.js
@@ -4,6 +4,11 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+//TEST
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from server' });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
 });
